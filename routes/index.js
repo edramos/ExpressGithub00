08b5d42874859c47341c64f9e0fa16a49e0682b6
Ed R <eduardorams@yahoo.com>
2016-06-02 20:46:41 -0500

    Added js and img folders

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,7 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/name/:user_name', function(req, res){
+  res.send('<html><body><h1>Hello ' + req.params.user_name + '</h1></body></html>');
+});
 module.exports = router;
